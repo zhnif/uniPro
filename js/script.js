f3c70b701eb274568694281b7557e0912d1c714d
zhnif <zaimsda@gmail.com>
2025-12-09 18:45:23 +0700

    updates zaim Sel/09/Des

--- js/script.js
+++ js/script.js
@@ -31,3 +31,49 @@ document.addEventListener('click', function (e) {
     navbarNav.classList.remove('active');
   }
 });
+
+// Highlight navbar menu on scroll
+const sections = document.querySelectorAll('section[id]');
+const navLinks = document.querySelectorAll('.navBar-nav a');
+
+window.addEventListener('scroll', () => {
+  let current = '';
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 120; // offset karena navbar fixed
+    const sectionHeight = section.offsetHeight;
+
+    if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+      current = section.getAttribute('id');
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove('active');
+
+    if (link.getAttribute('href') === `#${current}`) {
+      link.classList.add('active');
+    }
+  });
+});
+
+// Inisialisasi Owl Carousel untuk Team Section
+$(document).ready(function(){
+    $(".team-slider").owlCarousel({
+        loop: true,
+        margin: 20,
+        nav: true,
+        dots: true,
+        responsive:{
+            0:{
+                items: 1
+            },
+            600:{
+                items: 2
+            },
+            1000:{
+                items: 4
+            }
+        }
+    });
+});
